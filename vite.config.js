aa56d2f307fab4d8e46f8eb3a3dde8cbde973c16
pmancevski <pmancevskiwork@gmail.com>
2026-06-19 18:13:23 +0200

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
   },
   base: './'
 })
