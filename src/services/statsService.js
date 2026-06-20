83700676b710b1121d56f5e3da281aac9fb6b93d
pmancevski <pmancevskiwork@gmail.com>
2026-06-20 22:04:09 +0200

    testing fetching token 01

--- src/services/statsService.js
+++ src/services/statsService.js
@@ -4,15 +4,22 @@ import { fetchAuthSession } from 'aws-amplify/auth'
 const API_URL = 'https://3nhftt97bb.execute-api.eu-north-1.amazonaws.com/prod'
 
 export const fetchRealStats = async () => {
-  const session = await fetchAuthSession()
-  const token = session.tokens.accessToken.toString()
-  
-  const response = await axios.get(API_URL, {
-    headers: {
-      'Authorization': `Bearer ${token}`
-    }
-  })
-  return JSON.parse(response.data.body)
+  try {
+    const session = await fetchAuthSession()
+    const token = session.tokens.accessToken.toString()
+    
+    console.log('Token:', token) // Check if token is fetched
+    
+    const response = await axios.get(API_URL, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    return JSON.parse(response.data.body)
+  } catch (error) {
+    console.error('Error fetching token or stats:', error)
+    throw error
+  }
 }
 
 export const fetchRandomStats = () => {
